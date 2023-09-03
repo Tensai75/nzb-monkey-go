7587998d2c44b1c2e211ab18ed813631566aae1a
tuxxx <nzb_tuxxx@proton.me>
2023-09-03 18:53:48 +0200

    add comment in default cfg to show path to log file)

--- defaultconfig.go
+++ defaultconfig.go
@@ -13,7 +13,7 @@ categorize = "off"
 success_wait_time = 3
 # Seconds to wait befor ending/closing the window after an error
 error_wait_time = 10
-# Save output to logfile.txt
+# Write debug log to logfile.txt on windows/osx (same dir as nzb-monkey-go) or /tmp/nzb-monkey-go.log on linux
 debug = false
 
 [EXECUTE]
